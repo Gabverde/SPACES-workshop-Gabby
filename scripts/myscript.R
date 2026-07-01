69acdf59160be572f74be8a288e29d70dc60c13f
Emily Montes <emilymontes562@gmail.com>
2026-07-01 14:33:40 -0700

    made a boxplot, scatter, and histogram plot

--- scripts/myscript.R
+++ scripts/myscript.R
@@ -12,3 +12,35 @@ ggplot(complete_old, aes(x = hindfoot_length, y = weight, color = species))+
 # Make another comment
 ggplot(complete_old, aes(x = hindfoot_length, y = weight, color = species))+
   geom_jitter()
+
+
+# Check out these graph - by Emily
+# View Data
+View(complete_old)
+
+# Boxplot (sex v weight)
+ggplot(complete_old, aes(x = sex, y = weight, color = sex))+
+  geom_jitter(alpha = 0.15) +
+  geom_boxplot() +
+  labs(x = "Sex",
+       y = "Weight (g)",
+       title = "Boxplot of Sex by Weight") +
+  theme_classic()
+
+# Scatter plot (hindlength v weight)
+ggplot(complete_old, aes(x = hindfoot_length, y = weight))+
+  geom_jitter(alpha = 0.15) +
+  geom_point(size = 0.5, color = "purple", shape = 2) +
+  labs(x = "Hindfoot Lenght (cm)",
+       y = "Weight (g)") +
+  theme_bw()
+
+# Histogram
+# Great way to look at one variable is using a histogram
+ggplot(complete_old, aes(x = hindfoot_length)) +
+       geom_histogram()
+
+
+
+
+
